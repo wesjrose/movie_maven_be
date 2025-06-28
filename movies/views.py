0595b8541820dedec4f455d51097c0b540f006e1
Wes Rose <wesjrose@gmail.com>
2025-06-27 22:46:59 -0400

    feat: add basic parsing with next page

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,6 @@
 from django.http import JsonResponse
+from django.core.paginator import Paginator
+
 
 from rest_framework.views import APIView
 
@@ -15,12 +17,22 @@ class MoviesView(APIView):
         """
         This endpoint returns a list of movies
         """
+
+        page = int(request.query_params.get("page", 1))
+
         json_movie_data = None
         with open(
             os.path.join("movies", "data", "movie_data_2025_06_20.json"), "r"
         ) as f:
             json_movie_data = json.load(f)
 
-        movie_data = MovieSerializerFromJson(json_movie_data, many=True)
+        paginator = Paginator(json_movie_data, 20)
+        max_pages = paginator.num_pages
+        page_data = paginator.get_page(int(page))
+
+        next_page = page + 1 if (page + 1) <= max_pages else None
+
+        movie_data = MovieSerializerFromJson(page_data, many=True)
         movie_data = movie_data.data
-        return JsonResponse({"movies": movie_data}, status=200)
+
+        return JsonResponse({"movies": movie_data, "next_page": next_page}, status=200)
