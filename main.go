203aa541f0eebbfa0463a60ac5373901d1098480
Wes Rose <wesjrose@gmail.com>
2026-08-19 16:47:54 -0400

    feat: finish the first version of movie retreval

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,19 +19,48 @@ type Movie struct {
 	Rating float32 `json:"rating"`
 }
 
+// The following are TMDB urls that can be used for querying the API
+const MOVIE_DISCOVER string = "https://api.themoviedb.org/3/discover/movie"
+
+
 // get movies from external service and add them to the database
 func populateMovieData(c *gin.Context) {
-	_ = godotenv.Load()
-	tmdbToken := os.Getenv("TMDB_API_TOKEN")
-	fmt.Printf("The movie database token is: %s", tmdbToken)
-	c.JSON(http.StatusOK, gin.H{"message" : "hello world"})
+	
+	req, err := http.NewRequestWithContext(c, http.MethodGet, MOVIE_DISCOVER, nil)
+	if err != nil {
+		fmt.Printf("There was an error setting up the request to TMDB %s", err)
+	}
+	req.Header.Set("Authorization", "Bearer "+os.Getenv("TMDB_API_TOKEN"))
+	req.Header.Set("Accept", "application/json")
+	
+	q := req.URL.Query()
+	q.Set("primary_release_year", "2025")
+	q.Set("sort_by", "primary_release_date.desc")
+	req.URL.RawQuery = q.Encode()
+	
+	var client = &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	
+	resp, err := client.Do(req)
+	if err != nil {
+		c.JSON(resp.StatusCode, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+	
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+	
+	c.Data(resp.StatusCode, "application/json; charset=utf-8", body)
 	
 }
 
 func main() {
 	
-	// _ = godotenv.Load()
-	// tmdbToken = os.Getenv("TMDB_API_TOKEN")
+	_ = godotenv.Load()
 	
 	router := gin.Default()
 	router.GET("populate-movies", populateMovieData)
