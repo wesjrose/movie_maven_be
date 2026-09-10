558a67e1a67dea4d6816047e8fd4424c547c2692
Wes Rose <wesjrose@gmail.com>
2026-09-10 17:07:01 -0400

    refactor the api to include ORM support

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.26.5
 require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.12.3
+	gorm.io/driver/postgres v1.6.2
+	gorm.io/gorm v1.31.2
 )
 
 require (
@@ -31,6 +34,12 @@ require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/gohugoio/hashstructure v0.6.0 // indirect
 	github.com/gohugoio/hugo v0.164.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.10.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -51,6 +60,7 @@ require (
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.53.0 // indirect
 	golang.org/x/net v0.56.0 // indirect
+	golang.org/x/sync v0.21.0 // indirect
 	golang.org/x/sys v0.46.0 // indirect
 	golang.org/x/text v0.38.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
--- main.go
+++ main.go
@@ -2,74 +2,108 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-type Movie struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Genre string `json:"genre"`
-	Year int `json:"year"`
-	Rating float32 `json:"rating"`
-}
-
-// The following are TMDB urls that can be used for querying the API
 const TMDB_URL string = "https://api.themoviedb.org"
 const TMDB_DISCOVER string = "/3/discover/movie"
 
+var db *gorm.DB
 
-// get movies from external service and add them to the database
 func populateMovieData(c *gin.Context) {
-	
-	// req, err := http.NewRequestWithContext(c, http.MethodGet, MOVIE_DISCOVER, nil)
-	// if err != nil {
-	// 	fmt.Printf("There was an error setting up the request to TMDB %s", err)
-	// }
-	// req.Header.Set("Authorization", "Bearer "+os.Getenv("TMDB_API_TOKEN"))
-	// req.Header.Set("Accept", "application/json")
-	
-	// q := req.URL.Query()
-	// q.Set("primary_release_year", "2025")
-	// q.Set("sort_by", "primary_release_date.desc")
-	// q.Set("vote_count.gte", "50")
-	// q.Set("with_original_language", "en")
-	// req.URL.RawQuery = q.Encode()
-	
-	// var client = &http.Client{
-	// 	Timeout: 10 * time.Second,
-	// }
-	
-	params := make(map[string]string)
-	params["primary_release_year"] = "2025"
-	params["sort_by"] = "primary_release_date.desc"
-	params["vote_count.get"] = "50"
-	params["with_original_language"] = "en"
-	
-	headers := make(map[string]string)
-	headers["Authorization"] = "Bearer "+os.Getenv(("TMDB_API_TOKEN"))
-	headers["Accept"] = "application/json"
-	
+	params := map[string]string{
+		"primary_release_year":    "2025",
+		"sort_by":                 "primary_release_date.desc",
+		"vote_count.gte":          "50",
+		"with_original_language":  "en",
+	}
+
+	headers := map[string]string{
+		"Authorization": "Bearer " + os.Getenv("TMDB_API_TOKEN"),
+		"Accept":        "application/json",
+	}
+
 	client := NewHTTPClient(TMDB_URL)
 	resp, body, err := client.Get(c, TMDB_DISCOVER, params, headers)
-	
-	
-	if err != nil{
-		log.Fatal(err)
+	if err != nil {
+		c.JSON(http.StatusBadGateway, gin.H{"error": "failed to fetch movies from TMDB"})
+		return
 	}
-	
-	c.Data(resp.StatusCode, "application/json; charset=utf-8", body)
-	
+	if resp.StatusCode != http.StatusOK {
+		c.Data(resp.StatusCode, "application/json; charset=utf-8", body)
+		return
+	}
+
+	results, err := parseTMDBDiscover(body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to parse TMDB response"})
+		return
+	}
+
+	movies := make([]Movie, 0, len(results))
+	for _, result := range results {
+		movies = append(movies, result.toMovie())
+	}
+
+	if len(movies) > 0 {
+		err = db.Clauses(clause.OnConflict{
+			Columns: []clause.Column{{Name: "tmdb_id"}},
+			DoUpdates: clause.AssignmentColumns([]string{
+				"title",
+				"original_title",
+				"overview",
+				"release_date",
+				"poster_path",
+				"backdrop_path",
+				"original_language",
+				"vote_average",
+				"vote_count",
+				"popularity",
+				"adult",
+				"genre_ids",
+				"updated_at",
+			}),
+		}).Create(&movies).Error
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save movies"})
+			return
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"saved":  len(movies),
+		"movies": movies,
+	})
+}
+
+func listMovies(c *gin.Context) {
+	var movies []Movie
+	if err := db.Order("release_date DESC NULLS LAST").Find(&movies).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to load movies"})
+		return
+	}
+
+	c.JSON(http.StatusOK, movies)
 }
 
 func main() {
-	
 	_ = godotenv.Load()
-	
+
+	var err error
+	db, err = openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.Default()
-	router.GET("populate-movies", populateMovieData)
-	
+	router.GET("/populate-movies", populateMovieData)
+	router.GET("/movies", listMovies)
+
 	router.Run("localhost:8001")
 }
